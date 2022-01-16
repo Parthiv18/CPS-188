b983f575b801a25efd7569318e55f7a603cdef89
Parthiv18 <parthiv675@gmail.com>
2022-01-16 13:08:18 -0500

    first commit

---
+++ test1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 1;
+    int y = 993;
+
+    int sum = 0;
+    for (int i=4 ; i<=988 ;i++) {
+        sum += i;        
+    }
+    printf("%d", sum);
+    return 0;
+}
