4657d4567e60ade6c9154f563dcbedd5309d265a
Parthiv18 <parthiv675@gmail.com>
2022-01-16 13:10:14 -0500

    Update

--- test1.c
+++ test1.c
@@ -2,13 +2,6 @@
 
 int main() {
 
-    int x = 1;
-    int y = 993;
-
-    int sum = 0;
-    for (int i=4 ; i<=988 ;i++) {
-        sum += i;        
-    }
-    printf("%d", sum);
-    return 0;
+   printf("hello");
+   return 0;
 }
