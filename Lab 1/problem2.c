0a78e6d9666f7ff8a92739b158005438416d1dc9
Parthiv18 <parthiv675@gmail.com>
2022-02-13 14:58:33 -0500

    updated

--- Lab 1/problem2.c
+++ Lab 1/problem2.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main() {
+    int arr[] = {1, 2, 3, 4};
 
-int main () {
-    double num = 1232323.99999;
-    printf("%1.0lf",num);
-
-    return 0;
+    for (int i = 0; i < 4; i++) {          // loop size of array
+        for (int j = i; j < 4 - 1; j++) {  // loop one more
+            printf("%d %d\n", arr[i], arr[j]);
+        }
+    }
+    // printf("\n%d", counter); //4
+    // printf("\n%d", arr[5]); //size 4 so max 4
 }
