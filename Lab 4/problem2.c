4d02bc028847d586f30c00d3249836112e6460c7
Parthiv18 <parthiv675@gmail.com>
2022-02-13 22:52:41 -0500

    updated

--- Lab 4/problem2.c
+++ Lab 4/problem2.c
@@ -11,7 +11,7 @@ double grossPay(int hour, double wage) {
     }
 }  // round to nearst cent
 
-void output() {
+/*void output() {
     printf("\nEmp (1): (77621), Gross Pay: %.2lf\n", grossPay(35, 18.00));
     printf("Emp (2): (82010), Gross Pay: %.2lf\n", grossPay(16, 22.50));
     printf("Emp (3): (92390), Gross Pay: %.2lf\n", grossPay(46, 19.50));
@@ -20,7 +20,7 @@ void output() {
     printf("Emp (6): (19089), Gross Pay: %.2lf\n", grossPay(43, 16.00));
     printf("Emp (7): (54209), Gross Pay: %.2lf\n", grossPay(64, 17.00));
     printf("Emp (8): (50630), Gross Pay: %.2lf\n", grossPay(32, 20.00));
-}
+}*/
 
 int main() {
     /*
@@ -38,10 +38,9 @@ int main() {
     // testing
     while (fgets(line, sizeof(line), fp)) {
         sscanf(line, "%d %d %lf %d", &empNum, &shift, &wage, &hours);
-        //printf("%d %d %lf %d\n", empNum, shift, wage, hours);
-        printf("Employee Number: %d , Shifts: %d , Wage: %.2lf , Gross Pay: \n", empNum, shift, wage);
+        // printf("%d %d %lf %d\n", empNum, shift, wage, hours);
+        printf("Employee Number: %d , Shifts: %d , Wage: %.2lf , Gross Pay: $%.2lf\n", empNum, shift, wage, grossPay(hours, wage));
     }
-
     // output();
 
     fclose(fp);
