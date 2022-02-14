6cb6e274a4170bc2e7db386d056225f565b3c145
Parthiv18 <parthiv675@gmail.com>
2022-02-13 19:00:20 -0500

    edit

--- Lab 4/problem2.c
+++ Lab 4/problem2.c
@@ -1,2 +1,35 @@
 #include <math.h>
 #include <stdio.h>
+
+double grossPay(int hour, double wage) {
+    if (hour <= 15) {
+        return hour * wage;
+    } else if (hour > 15 && hour <= 25) {
+        return hour * (wage * 1.05);
+    } else if (hour > 25) {
+        return hour * (wage * 1.10);
+    }
+}  // round to nearst cent
+
+int main() {
+    /*
+        employee number (int)
+        number of shifts (int)
+        base wage rate (real)
+        one number per shift => number of hours worked
+    */
+
+    FILE* filePointer;
+    int bufferLength = 255;
+    char buffer[bufferLength]; /* not ISO 90 compatible */
+
+    filePointer = fopen("L4_data.txt", "r");
+
+    while (fgets(buffer, bufferLength, filePointer)) {
+        printf("%s", buffer);
+    }
+
+    fclose(filePointer);
+
+    return 0;
+}
