02c4ee7b762c8f929a49bbd236f691c685a77ba2
Parthiv18 <parthiv675@gmail.com>
2022-02-13 22:46:02 -0500

    test

--- Lab 4/problem2.c
+++ Lab 4/problem2.c
@@ -11,6 +11,17 @@ double grossPay(int hour, double wage) {
     }
 }  // round to nearst cent
 
+void output() {
+    printf("\nEmp (1): (77621), Gross Pay: %.2lf\n", grossPay(35, 18.00));
+    printf("Emp (2): (82010), Gross Pay: %.2lf\n", grossPay(16, 22.50));
+    printf("Emp (3): (92390), Gross Pay: %.2lf\n", grossPay(46, 19.50));
+    printf("Emp (4): (62396), Gross Pay: %.2lf\n", grossPay(12, 32.00));
+    printf("Emp (5): (89320), Gross Pay: %.2lf\n", grossPay(9, 27.50));
+    printf("Emp (6): (19089), Gross Pay: %.2lf\n", grossPay(43, 16.00));
+    printf("Emp (7): (54209), Gross Pay: %.2lf\n", grossPay(64, 17.00));
+    printf("Emp (8): (50630), Gross Pay: %.2lf\n", grossPay(32, 20.00));
+}
+
 int main() {
     /*
         employee number (int)
@@ -18,18 +29,22 @@ int main() {
         base wage rate (real)
         one number per shift => number of hours worked
     */
+    char line[1000];
+    FILE *fp = fopen("L4_data.txt", "r");
 
-    FILE* filePointer;
-    int bufferLength = 255;
-    char buffer[bufferLength]; /* not ISO 90 compatible */
+    int empNum, shift, hours;
+    double wage;
 
-    filePointer = fopen("L4_data.txt", "r");
-
-    while (fgets(buffer, bufferLength, filePointer)) {
-        printf("%s", buffer);
+    // testing
+    while (fgets(line, sizeof(line), fp)) {
+        sscanf(line, "%d %d %lf %d", &empNum, &shift, &wage, &hours);
+        //printf("%d %d %lf %d\n", empNum, shift, wage, hours);
+        printf("Employee Number: %d , Shifts: %d , Wage: %.2lf , Gross Pay: \n", empNum, shift, wage);
     }
 
-    fclose(filePointer);
+    // output();
+
+    fclose(fp);
 
     return 0;
 }
