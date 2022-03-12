6bbb7060d519bef50f24105761bb72d3141f98a1
Parthiv18 <parthiv675@gmail.com>
2022-03-12 12:05:57 -0500

    update

--- Lab 6/program1.c
+++ Lab 6/program1.c
@@ -2,29 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double min(double *arr, int size) {
+    double minVal = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < minVal) {
+            minVal = arr[i];
+        }
+    }
+    return minVal;
+}
+double max(double *arr, int size) {
+    double maxVal = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+    return maxVal;
+}
 double formula(double min, double max, double xi, double xlow, double xhigh) {  // function for formula
-    return min + (xi - xlow) * (max - min) / xhigh - xlow;
+    return min + (xi - xlow) * (max - min) / (xhigh - xlow);
 }
 
 int main() {
     // var
     FILE *fp;
     fp = fopen("data.txt", "r");
-    int start;
-    double min;
-    double max;
-    double *num;
+    int size;
+    double low, high;
 
     // getting info
-    fscanf(fp, "%d", &start);
-    fscanf(fp, "%lf", &min);
-    fscanf(fp, "%lf", &max);
+    fscanf(fp, "%d", &size);
+    fscanf(fp, "%lf", &low);
+    fscanf(fp, "%lf", &high);
+    double num[size];  // setting array to size
 
-    // printf("%d %.1lf %.1lf\n", start, min, max);
-
-    for (int i = 3; i < start + 3; i++) {  // starts at 3 to skip the first 3 elements since those are scanned | ends at 3 to consider remaining elements
-        fscanf(fp, "%lf ", &num[i]);       // scans values and puts it into an array
-        // printf("%.1lf ", num[i]);
-        printf("%.1lf ", formula(min, max, num[i], 0, start));  // output
+    for (int i = 0; fscanf(fp, "%lf", &num[i]) != EOF; i++) {  // loop to run down until end of file
+        printf("%.1lf ", num[i]);                              // printing normal array
+    }
+    printf("\nNormalized Array\n");
+    for (int j = 0; j < size; j++) {
+        printf("%.1lf ", formula(low, high, num[j], min(num, size), max(num, size)));
     }
 }
