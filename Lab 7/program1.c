47fed01d406e8615033516de87c330e65d401b0c
Parthiv18 <parthiv675@gmail.com>
2022-03-21 20:42:18 -0400

    update

--- Lab 7/program1.c
+++ Lab 7/program1.c
@@ -29,6 +29,22 @@ void reverse(char original[], char reversed[]) {
     }
 }
 
+void palindromeChecker(char original[], char reversed[]) {
+    int switchCounter = 1;
+    for (int i = 0; i < strlen(original); i++) {
+        if (original[i] == reversed[i]) {
+            switchCounter = 1;
+        } else {
+            switchCounter = 0;
+        }
+    }
+    if (switchCounter == 1) {
+        printf("same");
+    } else {
+        printf("not same");
+    }
+}
+
 int main() {
     char og[100];
     printf("string: ");     // asking user
@@ -37,12 +53,14 @@ int main() {
     char stp[100];  // empty strip array
     strip(og, stp);
     printf("Stripped Input: %s\n", stp);
-    printf("%d\n", strlen(stp));
+    // printf("%d\n", strlen(stp));
 
     char rev[strlen(stp) - 1];  // empty reverse array
     reverse(stp, rev);
     printf("Reversed String: %s\n", rev);
-    printf("%d\n", strlen(stp));
+    // printf("%d\n", strlen(stp));
+
+    palindromeChecker(stp, rev);  // checking if the same
 
     return 0;
 }
