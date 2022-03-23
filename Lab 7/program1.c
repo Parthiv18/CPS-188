461fa44ef8395a494b42a24a510d2ebb886fd763
Parthiv18 <parthiv675@gmail.com>
2022-03-23 16:05:45 -0400

    updated

--- Lab 7/program1.c
+++ Lab 7/program1.c
@@ -11,12 +11,11 @@ void strip(char original[], char stripped[]) {  // add strip
     }
 
     // checking if it has alpha or digits only
-    int j = 0, spaceCounter = 0;
-    while (original[j] != '\0') {
+    int spaceCounter = 0;
+    for (int j = 0; j < strlen(original); j++) {
         if ((isalpha(original[j]) || isdigit(original[j]))) {
             original[spaceCounter++] = original[j];
         }
-        j++;
     }
     original[spaceCounter] = '\0';
 
@@ -45,7 +44,7 @@ void compare(char original[], char reversed[]) {
             counter++;
         }
     }
-    // printf("\n\nCounter Char (stripped): [%d] Counter (same): [%d]\n", counterChar, counter); //if they are the same we want counterchar to be one more to match
+    // printf("\n\nCounter Char (stripped): [%d] Counter (same): [%d]\n", counterChar, counter);  // if they are the same we want counterchar to be one more to match
     if (counterChar + 1 == counter) {  // condition to check if they are palindrome
         printf("\nPalindrome");
     } else {
@@ -54,27 +53,17 @@ void compare(char original[], char reversed[]) {
 }
 
 int main() {
-    // char org[100] = "Marge lets Norah see Sharon's telegram.";  // original
-    // printf("Original: %s\n", org);
     char org[100];
     printf("Input String: ");  // asking user
     fgets(org, 100, stdin);    // storing string
 
     char orgStrip[100] = "\0";  // stripping original
     strip(org, orgStrip);
-    printf("Stripped: ");
-    for (int i = 0; i < 100; i++) {
-        printf("%c ", orgStrip[i]);
-    }
-
-    printf("\n");
+    printf("Stripped: %s\n", orgStrip);
 
     char rev[100] = "\0";  // reversing stripping original
     reverse(orgStrip, rev);
-    printf("Reversed: ");
-    for (int i = 0; i < 100; i++) {
-        printf("%c ", rev[i]);
-    }
+    printf("Reversed: %s", rev);
 
     compare(org, rev);
 
