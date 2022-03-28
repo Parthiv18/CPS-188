1d761fc7842db2e3371dbac27de5a3fd60a081a8
Parthiv18 <parthiv675@gmail.com>
2022-03-27 21:30:43 -0400

    updated

--- Lab 8/program1.c
+++ Lab 8/program1.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct address_t {
+    char ip[16];
+    char networkName[50];
+};
+
+void compare(char str1[], char str2[]) {
+    for (int i=0;i<10;i++) {
+        
+    }
+}
+
+void splitting(char str[]) {  // change it up [from google]
+    const char s[2] = ".";
+    char *token;
+
+    /* get the first token */
+    token = strtok(str, s);
+
+    /* walk through other tokens */
+    while (token != NULL) {
+        printf(" %s\n", token);
+
+        token = strtok(NULL, s);
+    }
+}
 
 int main() {
+    FILE *fp = fopen("data.txt", "r");
+    // address_t Network = ipAdd[16], networkName[50];
+    struct address_t Network[100];
+    int i = 0;
+
+    // strcpy(Network.ip, "test");
+    // scanning
+    while (fscanf(fp, "%s %s", &Network[i].ip, &Network[i].networkName) != EOF) {
+        i++;
+    }
+    for (int i = 0; i < 10; i++) {
+        // printf("%s - %s\n", Network[i].ip, Network[i].networkName);
+    }
+
+    printf("%s - %s\n", Network[0].ip, Network[1].networkName);
+    splitting(Network[0].ip);
+    printf("%s - %s\n", Network[0].ip, Network[1].networkName);
+    // printf("%s", Network.ip);
+
     return 0;
 }
