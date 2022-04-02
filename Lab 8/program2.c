42b6e001154f3f779c199f85da4d23682a98fb21
Parthiv18 <parthiv675@gmail.com>
2022-04-02 16:30:07 -0400

    updated

--- Lab 8/program2.c
+++ Lab 8/program2.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 //#include "mylibrary.h"
-/*output to bin file*/
 
 int main() {
     /*
         100 numbers -> [10][10]
+        output to bin file
     */
-    FILE *fp = fopen("real.txt", "r");
     double arr[10][10];
+    FILE *fp = fopen("real.txt", "r");
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; i < 10; j++) {
-            fscanf(fp, "%lf", &arr[i][j]);
+        for (int j = 0; j < 10; j++) {
+            fscanf(fp, "%lf ", &arr[i][j]);
         }
     }
 
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; i < 10; j++) {
-            printf("%lf\t", arr[i][j]);
+        for (int j = 0; j < 10; j++) {
+            printf("%.1lf\t", arr[i][j]);
         }
         printf("\n");
     }
